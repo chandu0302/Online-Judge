180bc0345005f079d77ff5cc3e0928307f09eb84
chandu0302 <chandu.kiran.budige@gmail.com>
2026-07-03 15:58:44 +0530

    deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "online-judge-m6vtxwo6z-chandu0302.vercel.app",
         changeOrigin: true,
       },
     },
